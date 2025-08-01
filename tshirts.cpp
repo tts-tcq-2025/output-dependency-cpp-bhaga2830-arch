232030c2e3129248b9932f038c679f1de7b4fcac
bhaga2830-arch <bhaga2830@gmail.com>
2025-08-01 10:17:47 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -3,11 +3,11 @@
 
 char size(int cms) {
     char sizeName = '\0';
-    if(cms < = 38) {
+    if(cms <= 38) { // Include 38 in the 'S' range
         sizeName = 'S';
-    } else if(cms > 38 && cms < = 42) {
+    } else if(cms > 38 && cms <= 42) { // Include 42 in the 'M' range
         sizeName = 'M';
-    } else {
+    } else { // No need to check cms > 42 explicitly
         sizeName = 'L';
     }
     return sizeName;
@@ -15,8 +15,14 @@ char size(int cms) {
 
 void testTshirtSize() {
     std::cout << "\nTshirt size test\n";
-    assert(size(37) == 'S');
-    assert(size(40) == 'M');
-    assert(size(43) == 'L');
+    assert(size(37) == 'S'); // Test for 'S'
+    assert(size(38) == 'S'); // Test boundary for 'S'
+    assert(size(40) == 'M'); // Test for 'M'
+    assert(size(42) == 'M'); // Test boundary for 'M'
+    assert(size(43) == 'L'); // Test for 'L'
     std::cout << "All is well (maybe!)\n";
 }
+
+int main() {
+    testTshirtSize();
+    return 0;
